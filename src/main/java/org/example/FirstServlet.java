692ff68460a6b03c7bba13d01f294db228e07bfe
Nezhdanov Sergei <tsebom@yandex.ru>
2022-04-11 00:13:07 +0300

    second commit

--- src/main/java/org/example/FirstServlet.java
+++ src/main/java/org/example/FirstServlet.java
@@ -1,5 +1,5 @@
 package org.example;
-
+//опять первый pull-request не удался
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
